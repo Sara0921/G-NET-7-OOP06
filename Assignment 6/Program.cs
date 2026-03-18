c21527d9b575e5962526028ec4f8347e6d2ef33f
Sara0921 <salahsara360@gmail.com>
2026-03-18 02:23:19 +0200

    Part01/Question01

--- Assignment 6/Program.cs
+++ Assignment 6/Program.cs
@@ -1,5 +1,21 @@
-﻿namespace Assignment_6
+﻿using System.ComponentModel;
+using System.Reflection.PortableExecutable;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assignment_6
 {
+    #region Part01
+    #region Question01
+    //Abstraction vs Encapsulation
+    //Abstraction is about hiding complexity — you show only what's necessary and hide the implementation details. It answers: "What does it do?"
+    //Encapsulation is about bundling and protecting data — you wrap data and methods together and restrict direct access.It answers: "How is it protected?"
+    //Example: A Coffee Machine
+    //Abstraction:
+    //Encapsulation:
+    //The water tank level, internal temperature, and bean count are stored privately inside the machine.You can't directly change them — you interact only through buttons, and the machine controls its own state.
+    #endregion
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
